007cd1932092d686efd2f099281c3c6ba895ea00
Marvel paul <djmarvel@yahoo.com>
2024-02-28 09:09:46 -0800

    binary tree

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,12 +1,12 @@
 #include "binary_trees.h"
 
 /**
- * * binary_tree_node - function that creates a binary tree node
- * * @parent:  function that creates a binary tree node
- * * @value: is the value to put in the new node
- * *
- * * Return: a pointer to the new node, or NULL on failure
- **/
+* binary_tree_node - function that creates a binary tree node
+* @parent:  function that creates a binary tree node
+* @value: is the value to put in the new node
+*
+* Return: a pointer to the new node, or NULL on failure
+*/
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
