8e7c483c48832408de179797ce5e519837ede0e0
Marvel paul <djmarvel@yahoo.com>
2024-02-28 18:15:01 -0800

    hearder file

--- binary_trees.h
+++ binary_trees.h
@@ -1,11 +1,11 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
-#include <stdlib.h>
-#include <stddef.h>
-#include <stdio.h>
-#include <math.h>
 
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
 
 /**
  *  * struct binary_tree_s - Binary tree node
@@ -15,24 +15,6 @@
  *      * @left: Pointer to the left child node
  *       * @right: Pointer to the right child node
  *        */
-typedef struct binary_tree_s
-{
-	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
-} binary_tree_t;
-
-void binary_tree_print(const binary_tree_t *);
-
-/**
- * * struct binary_tree_s - Binary tree node
- * *
- * * @n: Integer stored in the node
- * * @parent: Pointer to the parent node
- * * @left: Pointer to the left child node
- * * @right: Pointer to the right child node
- */
 struct binary_tree_s
 {
 	int n;
@@ -40,38 +22,34 @@ struct binary_tree_s
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
 };
-typedef struct binary_tree_s binary_tree_t;
 
-/*Binary Search Tree*/
+typedef struct binary_tree_s binary_tree_t;
+/* Binary Search Tree */
 typedef struct binary_tree_s bst_t;
 
-/*AVL Tree*/
+/* AVL Tree */
 typedef struct binary_tree_s avl_t;
 
-/*Max Binary Heap*/
+/* Max Binary Heap */
 typedef struct binary_tree_s heap_t;
 
-/*Binary tree print*/
+/* Printing helper function */
 void binary_tree_print(const binary_tree_t *);
 
 /**
- * * struct fifo_s - Structure representing a FIFO
- * * queue node for binary tree nodes
- * *
- * * @node: Pointer to a node of a binary tree
- * * @next: Pointer to the next element in the FIFO queue
- * * @prev: Pointer to the previous element in the FIFO queue
- */
-
-typedef struct fifo_s
+ *  * struct levelorder_queue_s - Level order traversal queue.
+ *   * @node: A node of a binary tree.
+ *    * @next: The next node to traverse to in the binary tree.
+ *     */
+typedef struct levelorder_queue_s
 {
-	const binary_tree_t *node;
-	struct fifo_s *next;
-	struct fifo_s *prev;
-} fifo_t;
+	binary_tree_t *node;
+	struct levelorder_queue_s *next;
+} levelorder_queue_t;
 
+/***************/
+/* Prototypes */
 
-/*Prototypes for every task*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
@@ -91,8 +69,7 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
-binary_tree_t *binary_trees_ancestor
-(const binary_tree_t *first, const binary_tree_t *second);
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_complete(const binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
@@ -103,4 +80,13 @@ bst_t *array_to_bst(int *array, size_t size);
 bst_t *bst_search(const bst_t *tree, int value);
 bst_t *bst_remove(bst_t *root, int value);
 int binary_tree_is_avl(const binary_tree_t *tree);
-#endif /* BINARY_TREES_H */
+avl_t *avl_insert(avl_t **tree, int value);
+avl_t *array_to_avl(int *array, size_t size);
+avl_t *avl_remove(avl_t *root, int value);
+avl_t *sorted_array_to_avl(int *array, size_t size);
+int binary_tree_is_heap(const binary_tree_t *tree);
+heap_t *array_to_heap(int *array, size_t size);
+int heap_extract(heap_t **root);
+int *heap_to_sorted_array(heap_t *heap, size_t *size);
+
+#endif /*BINARY_TREES_H
